11dcd20d18e77d90031f39e9a25f79fe11793056
Arkady Rosenberg <intelligent002@gmail.com>
2025-11-28 01:36:07 -0800

    path

--- query-haproxy.sh
+++ query-haproxy.sh
@@ -2,18 +2,18 @@
 set -euo pipefail
 
 curl -v --location 'https://node-00.intel.r7g.org:8300/' \
-  --cacert /data/cluster/certificates/CA/ca.crt \
-  --cert   /data/cluster/certificates/users/admin/admin.crt \
-  --key    /data/cluster/certificates/users/admin/admin.key \
+  --cacert ./ssl/CA/ca.crt \
+  --cert   ./ssl/users/admin/admin.crt \
+  --key    ./ssl/users/admin/admin.key \
   --tlsv1.3
 
 loop=2000
 for i in $(seq 1 $loop); do
   echo "------------------------------------------------------------ Request $i/$loop ----"
   curl --silent --show-error --location 'https://node-00.intel.r7g.org:8300/' \
-    --cacert /data/cluster/certificates/CA/ca.crt \
-    --cert   /data/cluster/certificates/users/admin/admin.crt \
-    --key    /data/cluster/certificates/users/admin/admin.key \
+    --cacert ./ssl/CA/ca.crt \
+    --cert   ./ssl/users/admin/admin.crt \
+    --key    ./ssl/users/admin/admin.key \
     --tlsv1.3 \
     --write-out "\n[Response Code: %{http_code}] [Time: %{time_total}s]\n"
 done
