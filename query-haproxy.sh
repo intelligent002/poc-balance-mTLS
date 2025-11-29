8d76dc1bf4cce60994ac11503f4c37a0ff6983ad
Arkady Rosenberg <intelligent002@gmail.com>
2025-11-29 03:43:44 -0800

    host required

--- query-haproxy.sh
+++ query-haproxy.sh
@@ -10,6 +10,7 @@ curl -v --location 'https://node-00.intel.r7g.org:8300/' \
 loop=2000
 for i in $(seq 1 $loop); do
   echo "------------------------------------------------------------ Request $i/$loop ----"
+  sleep 2
   curl --silent --show-error --location 'https://node-00.intel.r7g.org:8300/' \
     --cacert ./ssl/CA/ca.crt \
     --cert   ./ssl/users/admin/admin.crt \
